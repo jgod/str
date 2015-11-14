bf34687b172b4f0876d04bc4339f2991cb3013df
Justin Godesky <3506872+jgod@users.noreply.github.com>
2015-11-14 02:05:05 -0500

    More uniform formatting

--- tests/main.cpp
+++ tests/main.cpp
@@ -11,45 +11,38 @@
 #include "../str.h"
 using namespace jgod;
 
-TEST_CASE("position")
-{
-  SECTION("indexOf")
-  {
+TEST_CASE("position") {
+  SECTION("indexOf") {
     SECTION("empty strings returns -1") {REQUIRE(str::indexOf("", "search") == -1);}
     SECTION("empty substrings returns -1") {REQUIRE(str::indexOf("mystring", "") == -1);}
     SECTION("substrings longer than string return -1") {REQUIRE(str::indexOf("str", "string") == -1);}
     SECTION("not found returns -1") {REQUIRE(str::indexOf("mystring", "pizza") == -1);}
     SECTION("found returns index") {REQUIRE(str::indexOf("pizza pie", "pie") == 6);}
   }
-  SECTION("lastIndexOf")
-  {
+  SECTION("lastIndexOf") {
     SECTION("empty strings returns -1") {REQUIRE(str::lastIndexOf("", "search") == -1);}
     SECTION("empty substrings returns -1") {REQUIRE(str::lastIndexOf("mystring", "") == -1);}
     SECTION("substrings longer than string return -1") {REQUIRE(str::lastIndexOf("str", "string") == -1);}
     SECTION("not found returns -1") {REQUIRE(str::lastIndexOf("mystring", "pizza") == -1);}
     SECTION("found returns index") {REQUIRE(str::lastIndexOf("pizza pie", "p") == 6);}
   }
-  SECTION("startsWith")
-  {
+  SECTION("startsWith") {
     SECTION("does") {REQUIRE(str::startsWith("catdog", "cat"));}
     SECTION("doesn't") {REQUIRE(!str::startsWith("dog", "cat"));}
   }
-  SECTION("endsWith")
-  {
+  SECTION("endsWith") {
     SECTION("does") {REQUIRE(str::endsWith("catdog", "dog"));}
     SECTION("doesn't") {REQUIRE(!str::endsWith("cat", "dog"));}
   }
 }
 
-TEST_CASE("casing")
-{
+TEST_CASE("casing") {
   const auto SRC = "xx420xNoScOpe$";
   SECTION("to uppercase") {REQUIRE(str::toUpperCase(SRC) == "XX420XNOSCOPE$");}
   SECTION("to lowercase") {REQUIRE(str::toLowerCase(SRC) == "xx420xnoscope$");}
 }
 
-TEST_CASE("trimming")
-{
+TEST_CASE("trimming") {
   const auto SRC = "  mystring ";
   SECTION("left") {REQUIRE(str::trimLeft(SRC) == "mystring ");}
   SECTION("right")
@@ -60,27 +53,22 @@ TEST_CASE("trimming")
   SECTION("remove whitespace") {REQUIRE(str::removeWhitespace(SRC) == "mystring");}
 }
 
-TEST_CASE("splitting")
-{
+TEST_CASE("splitting") {
   const std::vector<std::string> RESULT = {"abcde", "fghi"};
   SECTION("based on line length") {REQUIRE(str::split("abcdefghi", 5) == RESULT);}
   SECTION("based on delimiter") {REQUIRE(str::split("abcde/fghi", "/") == RESULT);}
 }
 
-TEST_CASE("replacement")
-{
+TEST_CASE("replacement") {
   const auto SRC = "To be or not to be?";
 
-  SECTION("all occurrences with character")
-  {
+  SECTION("all occurrences with character") {
     REQUIRE(str::replace(SRC, "t", "b") == "To be or nob bo be?");
   }
-  SECTION("all occurrences with string")
-  {
+  SECTION("all occurrences with string") {
     REQUIRE(str::replace(SRC, "be", "test") == "To test or not to test?");
   }
-  SECTION("truncate")
-  {
+  SECTION("truncate") {
     REQUIRE(str::truncate(SRC, 8, "...") == "To be or...");
   }
 }
